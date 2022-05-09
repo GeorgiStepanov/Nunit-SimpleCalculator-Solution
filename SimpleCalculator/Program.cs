2d87632e0cfd8b74471b3d33b8e24e7ff1f2f512
GeorgiStepanov <georgi.stepanov@gmail.com>
2022-05-09 16:13:38 +0300

    Add the folder with the solution

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+    
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,68 @@
+﻿namespace SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public static class SimpleCalculator
+    {
+        public static decimal Sum(decimal[] array)
+        {
+
+            decimal sum = 0m;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        public static decimal Average(decimal[] array)
+        {
+            decimal sum = 0m;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            decimal averageValue = 0m;
+
+            return averageValue = sum / array.Length;
+        }
+
+        public static decimal Multiplication(decimal[] array)
+        {
+            decimal result = 1m;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result *= array[i];
+            }
+
+            return result;
+        }
+
+        public static decimal DevisionTwoNumbers(decimal dividend, decimal divisor)
+        {
+            decimal result = 0;
+
+            result = dividend / divisor;
+
+            return result;
+        }
+
+        public static decimal Subtraction(decimal[] numbers)
+        {
+            decimal result = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result -= numbers[i];
+            }
+
+            return result;
+        }
+    }
+}
